a6f543640f62fc4b686e8d9a6d27d3df8e415a5a
Karl-Heinz Marbaise <info@soebes.de>
2013-05-29 16:10:41 +0200

    - Enhanced unit test.

--- src/test/java/com/soebes/java/security/keystore/FirstTest.java
+++ src/test/java/com/soebes/java/security/keystore/FirstTest.java
@@ -2,12 +2,42 @@ package com.soebes.java.security.keystore;
 
 import static org.fest.assertions.Assertions.assertThat;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+
 import org.testng.annotations.Test;
 
-public class FirstTest {
+public class FirstTest extends TestBase {
 
 	@Test
 	public void firstTest() {
 		assertThat(true).isTrue();
+	}
+
+	@Test
+	public void firstSSLTest() throws KeyStoreException,
+			NoSuchAlgorithmException, CertificateException, IOException {
+		
+		File keyStoreFile = new File(getTestResourcesDirectory(), "keystore.pkcs12");
+
+		System.out.println("File:" + keyStoreFile);
+//		char[] password = "password".toCharArray();
+		String alias = "cn";
+
+		FileInputStream keyStoreInputStream = new FileInputStream(keyStoreFile);
+		KeyStore keystore = KeyStore.getInstance("pkcs12");
+		
+		keystore.load(keyStoreInputStream, null);
+
+		System.out.println("Type:" + keystore.getType());
+		Certificate cert = keystore.getCertificate(alias);
+		
+		System.out.println(cert);
 	}
 }
