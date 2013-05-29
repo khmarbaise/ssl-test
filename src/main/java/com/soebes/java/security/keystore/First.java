9e7c194eab9438f018a8e40c8866012374a1d77b
Karl-Heinz Marbaise <info@soebes.de>
2013-05-29 15:39:21 +0200

    - Added test classes.

---
+++ src/main/java/com/soebes/java/security/keystore/First.java
@@ -0,0 +1,15 @@
+package com.soebes.java.security.keystore;
+
+public class First {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/test/java/com/soebes/java/security/keystore/FirstTest.java
@@ -0,0 +1,13 @@
+package com.soebes.java.security.keystore;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+	@Test
+	public void firstTest() {
+		assertThat(true).isTrue();
+	}
+}
