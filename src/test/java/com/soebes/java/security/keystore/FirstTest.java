1d204f9db75a7dcecec8449b1a5adf5d4807b2d6
Karl-Heinz Marbaise <info@soebes.de>
2013-05-30 12:04:20 +0200

    - Reformatted code.

--- src/test/java/com/soebes/java/security/keystore/FirstTest.java
+++ src/test/java/com/soebes/java/security/keystore/FirstTest.java
@@ -13,74 +13,77 @@ import java.security.Provider;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
-import java.util.Enumeration;
+import java.util.Collections;
+import java.util.List;
 
 import org.testng.annotations.Test;
 
 public class FirstTest extends TestBase {
 
-	public static final char[] NO_PASSWORD = "".toCharArray();
-	public static final String KEY_STORE_TYPE = "PKCS12";
-	
-	public static final String KEY_STORE_FILE_NAME = "keystore.p12";
+    public static final char[] NO_PASSWORD = "".toCharArray();
 
-	@Test
-	public void firstTest() {
-		assertThat(true).isTrue();
+    public static final String KEY_STORE_TYPE = "PKCS12";
+
+    public static final String KEY_STORE_FILE_NAME = "keystore.p12";
+
+    @Test
+    public void firstTest() {
+	assertThat(true).isTrue();
+    }
+
+    @Test
+    public void firstSSLTest() throws KeyStoreException,
+	    NoSuchAlgorithmException, CertificateException, IOException,
+	    UnrecoverableKeyException {
+
+	File keyStoreFile = new File(getTestResourcesDirectory(),
+		KEY_STORE_FILE_NAME);
+
+	System.out.println("File:" + keyStoreFile);
+	// char[] password = "password".toCharArray();
+
+	FileInputStream keyStoreInputStream = new FileInputStream(keyStoreFile);
+	KeyStore keystore = KeyStore.getInstance(KEY_STORE_TYPE);
+
+	keystore.load(keyStoreInputStream, NO_PASSWORD);
+
+	System.out.println(" --------- AS LIST ---------------");
+	List<String> aliasesList = Collections.list(keystore.aliases());
+	for (String item : aliasesList) {
+	    System.out.println("Alias:" + item);
 	}
 
-	@Test
-	public void firstSSLTest() throws KeyStoreException,
-			NoSuchAlgorithmException, CertificateException, IOException, UnrecoverableKeyException {
-		
-		File keyStoreFile = new File(getTestResourcesDirectory(), KEY_STORE_FILE_NAME);
+	String certificateAlias = "cn";
 
-		System.out.println("File:" + keyStoreFile);
-//		char[] password = "password".toCharArray();
+	assertThat(keystore.containsAlias(certificateAlias)).isTrue();
+	assertThat(keystore.isKeyEntry(certificateAlias)).isTrue();
 
-		FileInputStream keyStoreInputStream = new FileInputStream(keyStoreFile);
-		KeyStore keystore = KeyStore.getInstance(KEY_STORE_TYPE);
-		
-		keystore.load(keyStoreInputStream, NO_PASSWORD);
+	Certificate cert = keystore.getCertificate(certificateAlias);
 
-		Enumeration<String> aliases = keystore.aliases();
+	System.out.println("Certificate:");
+	System.out.println("   Type: " + cert.getType());
+	System.out.println("   Cert: " + cert);
 
-		for ( ; aliases.hasMoreElements();) {
-			String item = aliases.nextElement();
-			System.out.println("Alias: '" + item + "'");
-		}
+	System.out.println("    X: " + cert.getPublicKey());
 
-		String certificateAlias = "cn";
+	Provider provider = keystore.getProvider();
 
-		assertThat(keystore.containsAlias(certificateAlias)).isTrue();
-		assertThat(keystore.isKeyEntry(certificateAlias)).isTrue();
+	System.out.println("Provider:");
+	System.out.println("     Name: " + provider.getName());
+	System.out.println("     Info: " + provider.getInfo());
+	System.out.println("  Version: " + provider.getVersion());
 
-		Certificate cert = keystore.getCertificate(certificateAlias);
-		
-		System.out.println("Certificate:");
-		System.out.println("   Type: " + cert.getType());
-		System.out.println("   Cert: " + cert);
-		
-		System.out.println("    X: " + cert.getPublicKey());
-		
-		Provider provider = keystore.getProvider();
-		
-		System.out.println("Provider:");
-		System.out.println("     Name: " + provider.getName());
-		System.out.println("     Info: " + provider.getInfo());
-		System.out.println("  Version: " + provider.getVersion());
-		
-//		Set<Entry<Object,Object>> entrySet = provider.entrySet();
+	// Set<Entry<Object,Object>> entrySet = provider.entrySet();
 
-		Key key = keystore.getKey(certificateAlias, NO_PASSWORD);
-		System.out.println("Key:");
-		System.out.println("  " + key.getFormat());
-		System.out.println("  " + key.getAlgorithm());
-		
-		
-//		Certificate[] certificateChain = keystore.getCertificateChain(certificateAlias);
-//		for (int i = 0; i < certificateChain.length; i++) {
-//			System.out.println("C: " + i + " " + );
-//		}
-	}
+	Key key = keystore.getKey(certificateAlias, NO_PASSWORD);
+	System.out.println("Key:");
+	System.out.println("  " + key.getFormat());
+	System.out.println("  " + key.getAlgorithm());
+
+	// Certificate[] certificateChain =
+	// keystore.getCertificateChain(certificateAlias);
+	// for (int i = 0; i < certificateChain.length; i++) {
+	// System.out.println("C: " + i + " " + );
+	// }
+    }
 }
